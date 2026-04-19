efb1eb76bb40f409028ec1dab09cb48e62579450
eyad yasser <eyad.learning@outlook.com>
2026-04-19 23:28:56 +0200

    changed frame size

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         JFrame frame = new JFrame("Shop test");
-        frame.setSize(1280,900);
+        frame.setSize(1280,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
        
